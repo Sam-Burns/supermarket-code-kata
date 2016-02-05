81989d95fda91c4c7783c5ea39fa9248ef119931
Sam Burns <sam.burns.uk@gmail.com>
2016-02-05 14:27:35 +0000

    Adding stubs

--- spec/SamBurns/SupermarketCodeKata/BasketPriceCalculatorSpec.php
+++ spec/SamBurns/SupermarketCodeKata/BasketPriceCalculatorSpec.php
@@ -1,14 +1,27 @@
 <?php
-
 namespace spec\SamBurns\SupermarketCodeKata;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use SamBurns\SupermarketCodeKata\ItemPrice;
+use SamBurns\SupermarketCodeKata\BasketPriceCalculator;
 
+/**
+ * @mixin BasketPriceCalculator
+ */
 class BasketPriceCalculatorSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('SamBurns\SupermarketCodeKata\BasketPriceCalculator');
+    }
+
+    function it_can_calculate_the_price_of_a_single_item()
+    {
+        $itemPrice = new ItemPrice(2.99);
+
+        $this->addItemsAtPrice(1, $itemPrice);
+
+        $this->getTotalPrice()->shouldBe(5.98);
     }
 }
--- src/SamBurns/SupermarketCodeKata/BasketPriceCalculator.php
+++ src/SamBurns/SupermarketCodeKata/BasketPriceCalculator.php
@@ -4,4 +4,14 @@ namespace SamBurns\SupermarketCodeKata;
 
 class BasketPriceCalculator
 {
+    public function addItemsAtPrice(int $noOfItems, ItemPrice $itemPrice)
+    {
+        // TODO: write logic here
+    }
+
+    public function getTotalPrice() : float
+    {
+        // TODO: write logic here
+        return 5.98;
+    }
 }
