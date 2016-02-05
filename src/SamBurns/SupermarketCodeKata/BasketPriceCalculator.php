4921b29c5ea8727782bca412ee9e9ebd0c170efa
Sam Burns <sam.burns.uk@gmail.com>
2016-02-05 17:03:40 +0000

    Adding more hints in code

--- src/SamBurns/SupermarketCodeKata/BasketPriceCalculator.php
+++ src/SamBurns/SupermarketCodeKata/BasketPriceCalculator.php
@@ -1,16 +1,36 @@
 <?php
 namespace SamBurns\SupermarketCodeKata;
 
+use SamBurns\SupermarketCodeKata\Item\Crisps;
+use SamBurns\SupermarketCodeKata\Item\Drink;
+use SamBurns\SupermarketCodeKata\Item\Sandwich;
+
 class BasketPriceCalculator
 {
+    /** @var Crisps[] */
+    private $crisps = [];
+
+    /** @var Drink[] */
+    private $drinks = [];
+
+    /** @var Sandwich[] */
+    private $sandwiches = [];
+
     public function addItem(ItemInterface $item)
     {
-        // TODO: write logic here
+        if ($item instanceof Crisps) {
+            $this->crisps[] = $item;
+        }
+        if ($item instanceof Drink) {
+            $this->drinks[] = $item;
+        }
+        if ($item instanceof Sandwich) {
+            $this->sandwiches[] = $item;
+        }
     }
 
     public function getTotalPrice() : float
     {
-        // TODO: write logic here
-        return 0.50;
+        /** @todo Implement this! */
     }
 }
